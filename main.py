8dd636a9394840f9165a2d65037c3329c0b89a59
KurkyOkurky <75952472+KurkyOkurky@users.noreply.github.com>
2023-03-11 15:49:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ from discord.ext import commands
 import json
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
-variace_nwordu = ["nigger", "negr", "nigga", "nigr", "nygr", "niggr", "nyggr"]
+word_count = ["Words here"]
 
 @bot.event
 async def on_ready():
     print(("We have logged in as {0.user}".format(bot)))
 
-# načtení counteru
+# Loads .json file for saving
 def load_counter():
     with open("counter.json", "r") as f:
         counters = json.load(f)
     return counters
 
-# uložení counteru
+# Saves data into .json file
 def save_counter(counters):
     with open("counter.json", "w") as f:
         json.dump(counters, f)
@@ -25,17 +25,17 @@ async def on_message(message):
     if message.author == bot.user:
         return
     users = []
-    # Checkne jestli zpráva obsahuje daný slovo
+    # Checks for certain words from list
     for n in variace_nwordu:
         if n in message.content.lower():
             counter = load_counter()
-            counter["nigger_count"] += 1
+            counter["counter"] += 1
             save_counter(counter)
-            await message.channel.send("Varianta slova nigger byla napsána " + (str(counter["nigger_count"])) + "krát")
+            await message.channel.send("TEXT " + (str(counter["counter"])) + "TEXT")
             for user in counter["users"]:
                 users.append(user[0])
 
-                # checkne jestli je user v listu
+                # Checks if user is in list, if not adds them
             if str(message.author) not in users:
                 counter["users"].append([str(message.author), 1])
                 await message.channel.send(1)
@@ -45,7 +45,7 @@ async def on_message(message):
                     if user[0] == str(message.author):
                         counter["users"][idx][1] += 1
                         await message.channel.send(
-                            str(message.author.mention) + " napsal " + str(counter["users"][idx][1]) + "krát n-word")
+                            str(message.author.mention) + " TEXT " + str(counter["users"][idx][1]) + "TEXT")
             save_counter(counter)
 
-bot.run("MTA3MDA2MTg3NjQyNTI2NTE2Mg.GZ5q6I.SH6WI0Zlyef3g5IDSSCT6-ilIKgVCJwdtKGnD0")
+bot.run("YOUR TOKEN HERE")
